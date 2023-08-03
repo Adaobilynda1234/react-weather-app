280f7d46f0c6c5f09a75e61b9071fb84eabc5d2e
Adaobilynda1234 <adaobilynda1234@gmail.com>
2023-08-03 13:57:24 +0100

    html structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https://github.com/Adaobilynda1234/react-weather-app.git"
-          target="_blank"
-        >
-          {" "}
-          Adaobi Lynda
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Adaobilynda1234/react-weather-app.git"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/Adaobilynda1234/react-weather-app.git"
+            target="_blank"
+          >
+            {" "}
+            Adaobi Lynda
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Adaobilynda1234/react-weather-app.git"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
